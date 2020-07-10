69f2171eee98e2a566ec433e1b450d66614fa9b8
Prahlad Panthi <prahlad1231@gmail.com>
2020-07-10 08:12:05 +0545

    Update SecurityConfiguration.java

--- src/main/java/com/prahlad/jpa/security/SecurityConfiguration.java
+++ src/main/java/com/prahlad/jpa/security/SecurityConfiguration.java
@@ -24,8 +24,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/admin").hasRole("ADMIN")
-		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/admin").hasAuthority("ADMIN")
+		.antMatchers("/user").hasAnyAuthority("USER", "ADMIN")
 		.antMatchers("/").permitAll()
 		.and().formLogin();
 	}
